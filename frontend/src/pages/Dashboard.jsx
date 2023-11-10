a2a145e6ed3df9dec6f42b132250a8f463615df1
luankrzyzaniak <luankrzyzaniak@gmail.com>
2023-11-10 01:21:41 -0300

    Almost finished dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import "./Dashboard.css"
-import { FaUserAlt } from "react-icons/fa";
+import { FaUserAlt, FaWarehouse, FaDollarSign, FaRegListAlt, FaHammer, FaSearch, FaLock } from "react-icons/fa";
+import { IoSettings } from "react-icons/io5";
 
 
 function Dashboard() {
@@ -11,23 +12,58 @@ function Dashboard() {
                 <div className="dashboard-headerSection">
                     <h1 className="dashboard-trademark">DJK Joias</h1>
                     <button className="dashboard-profileButton">
-                        <FaUserAlt className="dashboard-profileIcon"/>
+                        <FaUserAlt />
                         <br></br>
                         undefined
                     </button>
                 </div>
-                <div className="dashboard-headerSection">
-
+                <div className="dashboard-headerSection-alt">
+                    <div className="dashboard-input-section">
+                        <input placeholder="Pesquise algo legal"/>
+                        <button>
+                            <FaSearch className="dashboard-searchIcon"/>
+                         </button>
+                    </div>
                 </div>
-                <div className="dashboard-headerSection">
-
+                <div className="dashboard-headerSection-bottom" style={{ paddingLeft: '10vw', paddingRight: '10vw'}}>
+                    <button className="dashboard-iconsButton active">
+                        <FaWarehouse className="dashboard-Icons" />
+                    </button>
+                    <button className="dashboard-iconsButton">
+                        <FaDollarSign className="dashboard-Icons" />
+                    </button>
+                    <button className="dashboard-iconsButton">
+                        <FaHammer className="dashboard-Icons" />
+                    </button>
+                    <button className="dashboard-iconsButton">
+                        <IoSettings className="dashboard-Icons" />
+                    </button>
+                    <button className="dashboard-iconsButton">
+                        <FaRegListAlt className="dashboard-Icons" />
+                    </button>
                 </div>
             </div>
 
-            <main className="dashboard-main"></main>
-            
-            <footer className = "dashboard-footer">
-                <button className = "dashboard-footerButton">Sair do sistema</button>
+            <main className="dashboard-main">
+                <button>
+                    Consultar entradas e saídas
+                </button>
+                <button>
+                    Controle de vendas
+                </button>
+                <button>
+                    Controle de peças
+                </button>
+                <button>
+                    Compra de materiais
+                </button>                
+                <button>
+                    Entrega de serviço
+                </button>
+            </main>
+
+            <footer className="dashboard-footer">
+                <button style={{fontWeight:"bold"}}>Sair do sistema</button>
             </footer>
         </>
     );
