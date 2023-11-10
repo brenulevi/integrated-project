58663023ff93fc0119bdf16116dc890a88b37556
Luan Krzyzaniak <luankrzyzaniak@gmail.com>
2023-11-09 23:10:54 -0300

    Added page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,34 @@
+import axios from "axios";
+import "./Dashboard.css"
+import { FaUserAlt } from "react-icons/fa";
+
+
 function Dashboard() {
+    document.title = "Login - DJK Joias";
     return (
         <>
-            <h1>My Dashboard</h1>
+            <div className="dashboard-header">
+                <div className="dashboard-headerSection">
+                    <h1 className="dashboard-trademark">DJK Joias</h1>
+                    <button className="dashboard-profileButton">
+                        <FaUserAlt className="dashboard-profileIcon"/>
+                        <br></br>
+                        undefined
+                    </button>
+                </div>
+                <div className="dashboard-headerSection">
+
+                </div>
+                <div className="dashboard-headerSection">
+
+                </div>
+            </div>
+
+            <main className="dashboard-main"></main>
+            
+            <footer className = "dashboard-footer">
+                <button className = "dashboard-footerButton">Sair do sistema</button>
+            </footer>
         </>
     );
 }
