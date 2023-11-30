ebabd2a13b0ac18e37d39b4abad7c891f1e0ea22
Leparfait22 <helomeranda@gmail.com>
2023-11-30 17:25:54 -0300

    completed first crud

--- frontend/src/pages/Products/ProductsRegister.jsx
+++ frontend/src/pages/Products/ProductsRegister.jsx
@@ -11,12 +11,30 @@ function ProductsRegister() {
                             <FaArrowLeft />
                         </i>
                     </a>
-                    <h1 className="Profile-title">Cadastro de Produto</h1>
+                    <h1 className="Profile-title">Cadastro de Peça</h1>
                 </nav>
         </header>
         <main>
+            <h2 style={{paddingBottom:"1rem"}}>Informações da peça</h2>
             <form action="">
-                
+                <label for="model">Modelo:</label>
+                <input type="text" id="model"/>
+                <label for="materialdiv">Material:</label>
+                <div id="materialdiv" className="radio-div" style={{paddingTop:"0.5rem"}}>
+                    <input type="radio" id="prata"/>
+                    <label className="radio-labels"for="ouro18">Prata</label>
+                    <input type="radio" id="ouro10"/> 
+                    <label className="radio-labels"for="ouro10">Ouro 10k</label>
+                    <input type="radio" id="ouro18"/>
+                    <label className="radio-labels"for="ouro18">Ouro 18k</label>
+                </div>
+                <label for="weight">Peso:</label>
+                <input type="number" id="weight"/>
+                <label for="price">Valor:</label>
+                <input type="number" id="price"/>
+                <label for="model">Data de entrada:</label>
+                <input type="date" id="model"/>
+                <input type="submit" value="Cadastrar"/>
             </form>
         </main>
         <footer id="footer"></footer>
